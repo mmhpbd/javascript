f39d41e8bc49cff7bf1b34bb5676c8fe12e44525
mmhpbd <mmhparvez111@gmail.com>
2022-09-01 11:55:23 +0600

    add all

---
+++ loop/forloop.js
@@ -0,0 +1,14 @@
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast den valo kore khai.');
+//     console.log(roastGiven);
+// }
+
+// for(var i=0; i<=7; i++){
+//     console.log(i);
+// }
+
+//odd number
+
+for(var i=1; i<=20; i+=2){
+    console.log(i);
+}
---
+++ loop/wloopexample.js
@@ -0,0 +1,6 @@
+var number = 0; //loop variable.
+while(number <= 10 ){
+    console.log(number);
+    number++;
+
+}
