c11bd522f76d66939c26520fb0800a6786bc0869
mmhpbd <mmhparvez111@gmail.com>
2022-09-01 13:00:01 +0600

    add all

---
+++ loop/arrayLoop.js
@@ -0,0 +1,12 @@
+var number = [45, 86, 34, 25, 21, 65, 55];
+for(var i = 0; i < 7; i++){
+    var element = number[i];
+    console.log(element);
+}
+
+var element = ['hablu', 'kablu', 'sablu', 'peblu', 'teblu', 'zeblu']
+
+for(var i=0; i < element.length; i++){
+    ele = element[i];
+    console.log(ele);
+}
