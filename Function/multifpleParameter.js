5e8f902a5da592f00837318b323406d3a438a4de
mmhpbd <mmhparvez111@gmail.com>
2022-09-14 02:30:26 +0600

    final commit

---
+++ Function/multifpleParameter.js
@@ -0,0 +1,10 @@
+function addTwoNumber(number1, number2){
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+
+var firstNumebr = 35;
+var secondNumber = 47;
+var result = addTwoNumber(firstNumebr, secondNumber);
+console.log('result value:',result);
