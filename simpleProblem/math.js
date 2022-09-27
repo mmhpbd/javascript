7a9b33017ba1412f32c9354dfcdec83c59305c4f
mmhpbd <mmhparvez111@gmail.com>
2022-09-28 00:59:00 +0600

    final commit

--- simpleProblem/math.js
+++ simpleProblem/math.js
@@ -1,18 +1,31 @@
 // formula of Math.abs
-
 // const math = 6;
 // const output= Math.abs(math);
 // console.log(output);
 
 
 // formula of Math.ceil
-
 // const math = 7.45;
 // const output = Math.ceil(math);
 // console.log(output);
 
 
 //formula of Math.floor
-const math = 8.452;
-const output = Math.floor(math);
-console.log(output);
+// const math = 8.452;
+// const output = Math.floor(math);
+// console.log(output);
+
+
+// formula of Math.random
+// const output = Math.random()*6;
+// const rounded = Math.round(output);
+// console.log(rounded);
+
+
+// formula of Math.random with for loop
+
+for( var i = 0; i<=20; i++){
+    const output = Math.random()*6;
+const rounded = Math.round(output);
+console.log(rounded);
+}
